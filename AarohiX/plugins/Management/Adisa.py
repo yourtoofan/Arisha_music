ebd5407218a793d64534d95e5b69ac63740d8da6
yourtoofan <toofanpapa424@gmail.com>
2024-10-13 20:02:37 +0530

    Update Adisa.py

--- AarohiX/plugins/Management/Adisa.py
+++ AarohiX/plugins/Management/Adisa.py
@@ -85,7 +85,7 @@ channel = ["channel"]
 
 # ========================================= #
 
-@app.on_message(filters.command(["disa", "disha"], prefixes=["a", "A"]) & admin_filter)
+@app.on_message(filters.command(["arisha", "jaan"], prefixes=["a", "A"]) & admin_filter)
 async def restriction_app(app: app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
